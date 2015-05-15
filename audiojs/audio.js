58b10f5f86c98b20de570e4d8f78f578cefbd956
Bill Karkavos <BillKarkavos@users.noreply.github.com>
2015-05-15 17:56:41 +0100

    Update

--- audiojs/audio.js
+++ audiojs/audio.js
@@ -31,9 +31,9 @@
     // ### The main settings object
     // Where all the default settings are stored. Each of these variables and methods can be overwritten by the user-provided `options` object.
 	
-	//Workaround by removing autoplay attribute on the actual element and instead using autoplay:true in the javascript.
+	////Workaround by removing autoplay attribute on the actual element and instead using autoplay:true in the javascript.
     settings: {
-      autoplay: true,
+      autoplay: true, 
       loop: false,
       preload: true,
       imageLocation: path + 'player-graphics.gif',
